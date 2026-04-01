e4da3af6bc749a31661e98b9b39e99b8e71fb5e2
MaulDev <amadmaul13@gmail.com>
2026-04-01 14:17:10 +0700

    Update: content

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@ const API_BASE_URL = "https://simple-go-production.up.railway.app";
 const BASE_URL = location.hostname === "localhost" ||
                  location.hostname === "127.0.0.1"
     ? ""
-    : "/amaul";
+    : "/amaul/";
